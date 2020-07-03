91ec2308a614e31d9a1e793cff4ae58267bad700
Fellipe Fingoli <fellipe.fingoli@gmail.com>
2020-07-03 01:52:43 -0300

    Reafactor the lib's directories

---
+++ lib/file_fetcher.rb
@@ -0,0 +1,8 @@
+require "file_fetcher/version"
+require "file_fetcher/model/fetcher_resource"
+
+
+module FileFetcher
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/file_fetcher_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe FileFetcher do
+  it "has a version number" do
+    expect(FileFetcher::VERSION).not_to be nil
+  end
+end
